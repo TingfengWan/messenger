e4eb0c6c3d6a5822ce0317be9f87aac071317abd
Tingfeng Wan <tingfengwan@gmail.com>
2021-07-22 05:06:03 -0700

    changed procedure of read update

--- client/src/socket.js
+++ client/src/socket.js
@@ -4,7 +4,7 @@ import {
   setNewMessage,
   removeOfflineUser,
   addOnlineUser,
-  setReadMessages,
+  setReadMessage,
 } from "./store/conversations";
 
 const socket = io(window.location.origin);
@@ -22,8 +22,8 @@ socket.on("connect", () => {
   socket.on("new-message", (data) => {
     store.dispatch(setNewMessage(data.message, data.sender));
   });
-  socket.on("read-messages", (data) => {
-    store.dispatch(setReadMessages(data.messages, data.conversationId));
+  socket.on("read-message", (data) => {
+    store.dispatch(setReadMessage(data.message));
   });
 });
 
